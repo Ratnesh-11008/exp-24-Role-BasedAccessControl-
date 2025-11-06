c3fcd5e95e73fcae89fd228f6c7a267ec3b389e8
Ratnesh-11008 <anitasingh.br099@gmail.com>
2025-11-06 12:00:12 +0530

    Add role-based authorization middleware

---
+++ roleMiddleware.js
@@ -0,0 +1,12 @@
+function authorizeRoles(...allowedRoles) {
+  return (req, res, next) => {
+    if (!allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({
+        message: "Access denied. You do not have the required role.",
+      });
+    }
+    next();
+  };
+}
+
+module.exports = authorizeRoles;
